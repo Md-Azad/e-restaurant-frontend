6d0b8cdc10d05398565be04d3ddfb03113595000
Md-Azad <sokalazad@gmail.com>
2023-11-13 01:28:40 +0100

    added delete funtionality form myCart page

--- src/pages/Dashboard/MyCart/Mycart.jsx
+++ src/pages/Dashboard/MyCart/Mycart.jsx
@@ -1,17 +1,49 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Mycart = () => {
-  const [cart] = useCart();
+  const [cart,refetch] = useCart();
   const total = cart.reduce((sum, item) => item.price + sum, 0);
 
+  const handleDelete =(item) =>{
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`http://localhost:5000/carts/${item._id}`,{
+                method: 'DELETE'
+            })
+            .then(res=> res.json())
+            .then(data =>{
+                if(data.deletedCount>0){
+                    refetch();
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                      });
+
+                }
+            })
+          
+        }
+      });
+  }
+
   return (
-    <div>
+    <div className="w-full ml-4">
       <Helmet>
         <title>e-restaurant || My Cart</title>
       </Helmet>
-      <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+      <div className="uppercase font-semibold h-[60px] flex space-x-6 justify-evenly items-center">
         <h3 className="text-3xl">Total items: {cart.length}</h3>
         <h3 className="text-3xl">Total Price: $ {total}</h3>
         <button className="btn btn-warning btn-sm">Pay</button>
@@ -54,9 +86,9 @@ const Mycart = () => {
                 <td>
                   {item.name}
                 </td>
-                <td className="text-end">${item.price}</td>
+                <td className="text-left">${item.price}</td>
                 <td>
-                  <button className="btn btn-ghost btn-lg bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+                  <button onClick={()=>handleDelete(item)} className="btn btn-ghost btn-lg bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
                 </td>
               </tr>)
             }
