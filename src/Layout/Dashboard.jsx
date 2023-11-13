e9fe2667610f6e4d9db25916b6a81688478f229f
Md-Azad <sokalazad@gmail.com>
2023-11-13 01:34:34 +0100

    added item number beside my cart Navlink

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,8 +1,10 @@
 import { FaShoppingCart, FaWallet,FaCalendarAlt, FaHome } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 
 const Dashboard = () => {
+    const [cart] = useCart();
   return (
     <div className="drawer lg:drawer-open ">
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -20,7 +22,9 @@ const Dashboard = () => {
       <li><NavLink to='/dashboard/reservation'><FaCalendarAlt></FaCalendarAlt>Reservations</NavLink></li>
       <li><NavLink to='/dashboard/history'><FaWallet></FaWallet>Payment History</NavLink></li>
       <li>
-        <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart</NavLink>
+        <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart
+        <span className="badge badge-secondary">+{cart?.length}</span>
+        </NavLink>
       </li>
       <div className="divider"></div>
       <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
