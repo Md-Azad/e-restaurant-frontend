8bb9272a73c8fff2df4ff09e9c379b2655e794b1
Md-Azad <sokalazad@gmail.com>
2024-01-15 14:47:27 +0100

    updated api links

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,7 +4,7 @@ import useAxiosSecure from './useAxiosSecure';
 import useAuth from './useAuth';
 const useCart = () =>{
     const {user,loading} = useAuth();
-    // const token = localStorage.getItem('access-token');
+   
     const [axiosSecure] = useAxiosSecure();
 
 
